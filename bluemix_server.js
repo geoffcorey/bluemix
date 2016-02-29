9995bdeb65001b0b8c77566a13d55b9b8db5badb
Keith Morgan <jmorgan@us.ibm.com>
2016-02-29 15:09:16 -0500

    another try

--- bluemix_server.js
+++ bluemix_server.js
@@ -29,7 +29,7 @@ var getAccessToken = function (query) {
   var response;
   try {
     response = HTTP.post(
-      "https://uaa.eu-gb.bluemix.net/oauth/token", {
+      "https://login.eu-gb.bluemix.net/UAALoginServerWAR/oauth/authorize", {
         headers: {
           Accept: 'application/json',
           "User-Agent": userAgent
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'kizimo:bluemix',
-  version: '1.0.4',
+  version: '1.0.5',
   summary: 'IBM Bluemix OAuth flow',
   git: 'https://github.com/kizimo/bluemix.git',
   documentation: 'README.md'
